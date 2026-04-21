52699b732b4a9ea039f7abb411cfcd60ffb32ebd
Jullian Adriano <jullian.adriano@dhses.ny.gov>
2026-04-21 20:49:22 +0000

    messages now stored to array, for some reason

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -14,9 +15,10 @@ parser.add_argument("user_prompt", type=str, help="User prompt")
 def main():
     client = genai.Client(api_key=api_key)
     args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     response = client.models.generate_content(
         model="gemma-4-26b-a4b-it",
-        contents=args.user_prompt,
+        contents=messages,
     )
     if response.usage_metadata is None:
         raise RuntimeError("")
