52af3e89d74a9ae57859210e24cdf5800a4ac569
Jullian Adriano <jullian.adriano@dhses.ny.gov>
2026-04-21 20:58:14 +0000

    added verbose

--- main.py
+++ main.py
@@ -10,6 +10,18 @@ api_key = os.environ.get("GEMINI_API_KEY")
 
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+
+
+def verbose_output(args, response):
+    print(f"User Prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"{response.text}")
+
+
+def normal_output(response):
+    print(f"{response.text}")
 
 
 def main():
@@ -20,11 +32,13 @@ def main():
         model="gemma-4-26b-a4b-it",
         contents=messages,
     )
+
     if response.usage_metadata is None:
         raise RuntimeError("")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(response.text)
+    verbose_output(args, response) if args.verbose else normal_output(response)
+    # print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    # print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    # print(response.text)
 
 
 if __name__ == "__main__":
