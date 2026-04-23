ee52cd514436526ed10e2bac612fd0db64d1029e
Jullian Adriano <jullian.adriano@dhses.ny.gov>
2026-04-23 14:54:26 +0000

    added write functionality

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -7,7 +7,7 @@ def get_file_content(working_directory, file_path):
     try:
         working_dir_abs = os.path.abspath(working_directory)
         target_file = os.path.normpath(os.path.join(working_dir_abs, file_path))
-        print(target_file)
+        # print(target_file)
         if not os.path.isfile(target_file):
             return Exception(f'Error: "{file_path}" is not a file or does not exist.')
         if not os.path.commonpath([working_dir_abs, target_file]) == working_dir_abs:
--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -6,7 +6,7 @@ def get_files_info(working_directory, directory="."):
         working_dir_abs = os.path.abspath(working_directory)
         target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
         if not os.path.isdir(target_dir):
-            return Exception('Error: "{directory}" is not a directory')
+            return Exception(f'Error: "{directory}" is not a directory')
         if not os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs:
             return Exception(
                 f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
